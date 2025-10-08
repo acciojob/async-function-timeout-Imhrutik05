cf73641b750f12a7d2de85418ea6102cde1493ed
Hrutik Chaudhari <hrutikchaudhari05@gmail.com>
2025-10-08 14:12:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
-//your JS code here. If required.
+// select 
+const msg = document.getElementById('text').trim();
+const delay = document.getElementById('delay');
+const btn = document.getElementById('btn');
+const msgDiv = document.getElementById('output');
+
+async function returnMsg(){
+	try {
+		if(msg && delay){
+			await delayMsg(msg, delay);
+			msgDiv.style.display = 'block';
+			msgDiv.textContent = `${msg}`;
+		} else {
+			throw new Error("Input fields are empty!");
+		}
+	} catch (error) {
+		console.log(error.message || error);
+	}	
+}
+
+function delayMsg(msg, delay){
+	return new Promise((resolve) => {
+		setTimeout(() => {
+			resolve(msg);
+		}, delay)
+	})
+}
+
+
+btn.addEventListener('click', returnMsg);
